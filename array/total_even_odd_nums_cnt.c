44cdf249b7c5e3c7a111379171c6cc2229c46dce
anthonyreddy21 <52660784+anthonyreddy21@users.noreply.github.com>
2021-05-21 13:54:32 +0530

    Update total_even_odd_nums_cnt.c

--- array/total_even_odd_nums_cnt.c
+++ array/total_even_odd_nums_cnt.c
@@ -1,6 +1,7 @@
 // Write a C program to count total number of even and odd elements in an array.
 
 #include <stdio.h>
+
 #define MAX 20
 
 int main()
@@ -10,15 +11,22 @@ int main()
    int even_cnt=0;
    int odd_cnt=0;
    int a[MAX];
+   
    printf("Enter array size\n");
    scanf("%d",&size);
+   
    printf("Enter array\n");
+   
    for(i=0;i<size;i++)
-   scanf("%d",a+i);
+      scanf("%d",a+i);
+   
    for(i=0;i<size;i++)
-  if(a[i]%2==0)
-  even_cnt++;
-  else
-  odd_cnt++;
+      if(a[i]%2==0)
+         even_cnt++;
+      else
+         odd_cnt++;
+   
    printf("even numbers count is %d,odd numbers count %d",even_cnt,odd_cnt);
+
 }
+
